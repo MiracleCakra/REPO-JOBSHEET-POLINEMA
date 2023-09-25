24827c8469093f5fe0bcadcbd9e3facdef98514e
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2023-09-25 19:28:35 +0700

    percobaan 2

--- PemilihanPercobaan207.java
+++ PemilihanPercobaan207.java
@@ -3,17 +3,34 @@ import java.util.Scanner;
 public class PemilihanPercobaan207 {
     public static void main(String[] args) {
         Scanner input07 = new Scanner(System.in);
-        System.out.print("Nilai uas: ");
+        System.out.print("Nilai UAS: ");
         float uas = input07.nextFloat();
-        System.out.print("Nilai uts : ");
+        System.out.print("Nilai UTS: ");
         float uts = input07.nextFloat();
-        System.out.print("Nilai kuis : ");
+        System.out.print("Nilai kuis: ");
         float kuis = input07.nextFloat();
-        System.out.print("Nilai tugas : ");
+        System.out.print("Nilai Tugas: ");
         float tugas = input07.nextFloat();
-
         float total = (uas * 0.4f) + (uts * 0.3f) + (kuis * 0.1f) + (tugas * 0.2f);
+
         String message = total < 65 ? "Remidi" : "Tidak Remidi";
-        System.out.println("Nilai akhir = " + total + " sehingga " + message);
+
+        if (total >= 80 && total <= 100) {
+            System.out.println("Nilai huruf: A");
+        } else if (total >= 73 && total < 80) {
+            System.out.println("Nilai huruf: B+");
+        } else if (total >= 70 && total < 73) {
+            System.out.println("Nilai huruf: B");
+        } else if (total >= 65 && total < 70) {
+            System.out.println("Nilai huruf: C+");
+        } else if (total >= 60 && total < 65) {
+            System.out.println("Nilai huruf: C");
+        } else if (total >= 50 && total < 60) {
+            System.out.println("Nilai huruf: D");
+        } else { // total < 50
+            System.out.println("Nilai huruf: E");
+        }
+
+        System.out.println("Nilai akhir: " + total + " sehingga " + message);
     }
 }
---
+++ emilihanPercobaan307.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan307 {
+    public static void main(String[] args) {
+
+        Scanner input07 = new Scanner(System.in);
+        double angka1, angka2, hasil;
+        char operator;
+
+        Scanner input08 = new Scanner(System.in);
+        System.out.print("Masukkan angka pertama: ");
+        angka1 = input08.nextDouble();
+        System.out.print("Masukkan angka kedua: ");
+        angka2 = input08.nextDouble();
+        System.out.print("Masukkan operator: ");
+        operator = input08.next().charAt(0);
+
+        switch (operator) {
+            case '+':
+                System.out.println(angka1 + " + " + angka2 + " = " + (angka1 + angka2));
+                break;
+            case '-':
+                System.out.println(angka1 + " - " + angka2 + " = " + (angka1 - angka2));
+                break;
+            case '*':
+                System.out.println(angka1 + " * " + angka2 + " = " + (angka1 * angka2));
+                break;
+            case '/':
+                System.out.println(angka1 + " / " + angka2 + " = " + (angka1 / angka2));
+                break;
+            default:
+                System.out.println("Operator salah");
+        }
+    }
+}
