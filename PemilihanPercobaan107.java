4d053dbc6bc13264c654a7e11d3ad57ae0c18016
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2023-09-25 18:03:41 +0700

    PemilihanPerocbaan107

---
+++ PemilihanPercobaan107.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class PemilihanPercobaan107 {
+    public static void main(String[] args){
+        Scanner input07 = new Scanner(System.in);
+        System.out.print("Masukkan angka: ");
+        int angka = input07.nextInt();
+        if (angka % 2 == 0){
+            System.out.println("Angka " + angka + " adalah bilangan genap");
+        } else {
+            System.out.println("Angka " + angka + " adalah bilangan ganjil");
+        }
+        }
+    }
