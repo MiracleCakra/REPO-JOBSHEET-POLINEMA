be4ed0eb1087910a544032a6e9fce3cdb680c608
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2023-09-25 18:39:23 +0700

    PemilihanPercobaan107 Tennary Operator

--- PemilihanPercobaan107.java
+++ PemilihanPercobaan107.java
@@ -1,13 +1,12 @@
 import java.util.Scanner;
+
 public class PemilihanPercobaan107 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input07 = new Scanner(System.in);
         System.out.print("Masukkan angka: ");
         int angka = input07.nextInt();
-        if (angka % 2 == 0){
-            System.out.println("Angka " + angka + " adalah bilangan genap");
-        } else {
-            System.out.println("Angka " + angka + " adalah bilangan ganjil");
-        }
-        }
+        
+        String jenisBilangan = (angka % 2 == 0) ? "genap" : "ganjil";
+        System.out.println("Angka " + angka + " adalah bilangan " + jenisBilangan);
     }
+}
