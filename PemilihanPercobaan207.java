af04e69e53578912f4d2a138668d96070de2127a
Cakra Wangsa M.A.W <143616053+MiracleCakra@users.noreply.github.com>
2023-09-26 17:07:50 +0700

    PemilihanPercobaan207.java

--- PemilihanPercobaan207.java
+++ PemilihanPercobaan207.java
@@ -3,34 +3,39 @@ import java.util.Scanner;
 public class PemilihanPercobaan207 {
     public static void main(String[] args) {
         Scanner input07 = new Scanner(System.in);
-        System.out.print("Nilai UAS: ");
+        System.out.print("Nilai uas     : ");
         float uas = input07.nextFloat();
-        System.out.print("Nilai UTS: ");
+        System.out.print("Nilai uts     : ");
         float uts = input07.nextFloat();
-        System.out.print("Nilai kuis: ");
+        System.out.print("Nilai kuis    : ");
         float kuis = input07.nextFloat();
-        System.out.print("Nilai Tugas: ");
+        System.out.print("Nilai tugas   : ");
         float tugas = input07.nextFloat();
-        float total = (uas * 0.4f) + (uts * 0.3f) + (kuis * 0.1f) + (tugas * 0.2f);
 
-        String message = total < 65 ? "Remidi" : "Tidak Remidi";
+        float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
+        String message = total < 65 ? "Remidi" : "Tidak remidi";
 
-        if (total >= 80 && total <= 100) {
-            System.out.println("Nilai huruf: A");
-        } else if (total >= 73 && total < 80) {
-            System.out.println("Nilai huruf: B+");
-        } else if (total >= 70 && total < 73) {
-            System.out.println("Nilai huruf: B");
-        } else if (total >= 65 && total < 70) {
-            System.out.println("Nilai huruf: C+");
-        } else if (total >= 60 && total < 65) {
-            System.out.println("Nilai huruf: C");
-        } else if (total >= 50 && total < 60) {
-            System.out.println("Nilai huruf: D");
-        } else { // total < 50
-            System.out.println("Nilai huruf: E");
+        if (total > 80 && total <= 100) {
+            System.out.println("Nilai akhir = " + total + " sehingga " + message);
+            System.out.println("Nilai huruf = A (Sangat Baik)" );
+        } else if (total > 73 && total <= 80) {
+            System.out.println("Nilai akhir = " + total + " sehingga " + message);
+            System.out.println("Nilai huruf = B+ (Lebih dari Baik)" );
+        } else if (total > 65 && total <= 73) {
+            System.out.println("Nilai akhir = " + total + " sehingga " + message);
+            System.out.println("Nilai huruf = B (Baik)" );
+        } else if (total > 60 && total <= 65) {
+            System.out.println("Nilai akhir = " + total + " sehingga " + message);
+            System.out.println("Nilai huruf = C+ (Lebih dari Cukup)" );
+        } else if (total > 50 && total <= 60) {
+            System.out.println("Nilai akhir = " + total + " sehingga " + message);
+            System.out.println("Nilai huruf = C (Cukup)" );
+        } else if (total > 39 && total <= 50) {
+            System.out.println("Nilai akhir = " + total + " sehingga " + message);
+            System.out.println("Nilai huruf = D (Kurang)" );
+        } else if (total <= 39) {
+            System.out.println("Nilai akhir = " + total + " sehingga " + message);
+            System.out.println("Nilai huruf = E (Gagal)" );
         }
-
-        System.out.println("Nilai akhir: " + total + " sehingga " + message);
     }
 }
